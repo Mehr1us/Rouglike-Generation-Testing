81d96693c998fd9a769ad129d03da9a74b643040
Marius <74598551+Mehr1us@users.noreply.github.com>
2020-11-19 22:36:06 +0000

    Updated Comments

--- main.h
+++ main.h
@@ -4,6 +4,8 @@ static const bool liteDebug = false; // only if debug = false;
 static const bool showGen = false;
 //change showGen to true when debug = false; to see generation each of the 200 steps at a time (slow)
 
+//stops me from needing to include all of these in every .cpp file
+//don't add to other header files
 #include <stdio.h>
 #include "DWCA.h"
 #include "Tools.h"
