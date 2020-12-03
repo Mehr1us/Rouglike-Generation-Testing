38ae8da6bb442785f69459f02c8352e3c75bd568
Marius <74598551+Mehr1us@users.noreply.github.com>
2020-12-03 09:15:14 +0000

    Added Comments

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include "main.h"
 
+//classes (look at CAutomata.h and PathTest.h respectively)
 CAutomata cAuto;
 PathTest pathT;
 
 void main()
 {
-
+	//ask for which Generation to showcase/test
 	int input;
 	printf("Path Generation(0) or Cellular Automata(1)?\n");
 	scanf_s("%d", &input);
