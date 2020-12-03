3852ccd3ed37099bd172e5e00c5cbfbae7f9a2f6
Marius <74598551+Mehr1us@users.noreply.github.com>
2020-12-03 09:16:29 +0000

    Added Comments

--- CAutomata.h
+++ CAutomata.h
@@ -1,8 +1,7 @@
-
+//a class for CAutomata so I could call the functions from outside of CAutomata.cpp
 class CAutomata {
 public:
 	void generate();
 	void getSeed(char seed[40]);
 	void draw(unsigned char map[40][150]);
-protected:
 };
