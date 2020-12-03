b9ab2802cbcaee3e6c16ad18f76702802980d414
Marius <74598551+Mehr1us@users.noreply.github.com>
2020-12-03 09:28:50 +0000

    Added Comments

--- PathTest.h
+++ PathTest.h
@@ -5,16 +5,21 @@ public:
 	int x, y;
 	int id;
 	int roomSize;
-
+	
+	//Constructor of a "Point"
 	Point(int x, int y, int id, int roomSize);
 };
 
 class Path {
 public:
+	//adjustable list of Points (which contain 4 ints each)
 	std::vector<Point*>points;
 	Path();
 };
 
+//class for calling functions outside of PathTest.cpp
+//everything but generate should prob be not in public but in protected
+//but I couldn't be bothered
 class PathTest {
 public:
 	void getSeed(char seed[40]);
